8d2ddc8723a5ed7f6e225391723d23303b7a57fd
rangoPipe <farango@ideassimples.com.co>
2019-06-17 14:28:41 -0500

    Se reinicializa en Angular8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
-import { APP_ROUTING } from './app.routes';
-
-import { HeroesService } from './services/heroes.service';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { HeroeComponent } from './components/heroes/heroe.component';
-
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    APP_ROUTING,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [ HeroesService ],
-  bootstrap: [ AppComponent ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
